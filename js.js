15d00166b72a3cd8fb03fc1b1d96ea1c3387df2a
Lisy26 <elii.nesterzak@gmail.com>
2023-03-01 00:02:06 -0300

    CV final

---
+++ js.js
@@ -0,0 +1,18 @@
+var boton = "";
+function verImagen(boton){
+    let button = document.getElementById(boton);
+    button.hidden = false;
+}
+function ocultarImagen(boton){
+    let button = document.getElementById(boton);
+    button.hidden = true;
+}
+/*
+let buttonAccenture = document.getElementById("accenture");
+function verImagen(){
+        buttonAccenture.hidden = false;
+}
+
+function ocultarImagen(){
+    buttonAccenture.hidden = true;
+}*/
